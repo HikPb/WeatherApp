9165ca973f802b7a454cb6627bf1c195b7b3eadc
HikPb <p.ngocba@gmail.com>
2020-04-04 15:02:59 +0700

    react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { getAllByPlaceholderText } from '@testing-library/react';
 
+const api = {
+  key: "dskflsajflsdkfew109339fsdlk",
+  base: "https://api.openweathermap.org/data/2.5"
+}
 function App() {
+  const getDateNow = (d)=>{
+    let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday"]
+    let months=["January", "February", "March", "April", "May", "June", "July", "August", "September", "Octorber", "November", "December"]
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app warm">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..."/>
+        </div>
+        <div className="location-box">
+          <div className="location">Nam Dinh, Viet Nam</div>
+          <div className="date">{getDateNow(new Date())}</div>
+        </div>
+      </main>
     </div>
   );
 }
